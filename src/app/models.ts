da0574400edf4c27483037db0b9525db6fc68095
SirNoob97 <martinald467@gmail.com>
2022-06-11 21:58:33 +0200

    complete 1h tutorial

---
+++ src/app/models.ts
@@ -0,0 +1,50 @@
+export interface Game {
+  id: string,
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: number;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatform>;
+  publishers: Array<Publisher>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshot>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+
+interface Genre {
+  name: String;
+}
+
+interface ParentPlatform {
+  platform: {
+    slug: String;
+  };
+}
+
+interface Publisher {
+  name: string;
+}
+
+interface Rating {
+  id: number;
+  count: number;
+  title: string;
+}
+
+interface Screenshot {
+  image: string;
+}
+
+interface Trailer {
+  data: {
+    max: string;
+  };
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_URL: 'https://api.rawg.io/api',
+  API_KEY: '382d5dcc609b4d22bf2463796fa9dd68'
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
+ * For asier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
